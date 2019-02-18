4836c73cc1eb63e18b4b05dc11d7ac1d8346ef21
riley-moss <rileypmoss@gmail.com>
2019-02-17 20:09:51 -0600

    Finished test for pencil running out during writing, returning input plus a partial string

--- src/main/java/com/pillar/Pencil.java
+++ src/main/java/com/pillar/Pencil.java
@@ -5,7 +5,7 @@ public class Pencil {
 	private int initialPointDurability;
 	private int currentPointDurability;
 	private int eraserDurability;
-	
+
 	public Pencil(int pencilLength, int pointDurability, int eraserDurability) {
 		this.pencilLength = pencilLength;
 		this.initialPointDurability = pointDurability;
@@ -16,7 +16,7 @@ public class Pencil {
 	public int getPointDurability() {
 		return currentPointDurability;
 	}
-	
+
 	public int getLength() {
 		return pencilLength;
 	}
@@ -24,20 +24,18 @@ public class Pencil {
 	public int getEraserDurability() {
 		return eraserDurability;
 	}
-	
+
 	public String write(String input, String adder) {
 		String lettersAdded = "";
-		for(int i = 0, n = adder.length(); i < n; i++) {
+		for (int i = 0, n = adder.length(); i < n; i++) {
 			char letter = adder.charAt(i);
-			if(Character.isLowerCase(letter) && currentPointDurability > 0) {
+			if (Character.isLowerCase(letter) && currentPointDurability > 0) {
 				currentPointDurability--;
 				lettersAdded += letter;
-			}
-			else if(Character.isUpperCase(letter) && currentPointDurability > 1) {
+			} else if (Character.isUpperCase(letter) && currentPointDurability > 1) {
 				currentPointDurability -= 2;
 				lettersAdded += letter;
-			}
-			else if (letter == ' ') {
+			} else if (letter == ' ') {
 				lettersAdded += letter;
 			}
 		}
@@ -45,7 +43,7 @@ public class Pencil {
 	}
 
 	public void sharpen() {
-		if(pencilLength > 0) {
+		if (pencilLength > 0) {
 			currentPointDurability = initialPointDurability;
 			pencilLength--;
 		}
@@ -53,16 +51,15 @@ public class Pencil {
 
 	public String erase(String input, String erasedWord) {
 		int index = input.lastIndexOf(erasedWord);
-		if(index > 0 && erasedWord != "" && eraserDurability > 0) {
+		if (index > 0 && erasedWord != "" && eraserDurability > 0) {
 			String updatedInput = input.substring(0, index);
 			String temp = "";
-			for(int i = erasedWord.length()-1; i > -1; i--) {
-				if(eraserDurability > 0) {
+			for (int i = erasedWord.length() - 1; i > -1; i--) {
+				if (eraserDurability > 0) {
 					temp = " " + temp;
-					if(erasedWord.charAt(i) != ' ')
+					if (erasedWord.charAt(i) != ' ')
 						eraserDurability--;
-				}
-				else
+				} else
 					temp = erasedWord.charAt(i) + temp;
 			}
 			updatedInput += temp + input.substring(index + erasedWord.length());
@@ -74,14 +71,12 @@ public class Pencil {
 	public String edit(String input, String edits) {
 		int editLength = edits.length();
 		int blankSpaceStart = findWhiteSpace(input);
-		if(editLength > 0 && blankSpaceStart != -1) {
+		if (editLength > 0 && blankSpaceStart != -1) {
 			String updatedInput = input.substring(0, blankSpaceStart);
-			for (int i = 0; i < editLength; i ++) {
-				if(input.charAt(blankSpaceStart + i) == ' ') {
+			for (int i = 0; i < editLength; i++) {
+				if (input.charAt(blankSpaceStart + i) == ' ') {
 					updatedInput += edits.charAt(i);
-				}
-				else
-				{
+				} else {
 					updatedInput += "@";
 				}
 			}
@@ -92,8 +87,8 @@ public class Pencil {
 	}
 
 	private int findWhiteSpace(String input) {
-		for(int i = 1, n = input.length(); i < n; i++) {
-			if(input.charAt(i-1) == ' ' && input.charAt(i) == ' ')
+		for (int i = 1, n = input.length(); i < n; i++) {
+			if (input.charAt(i - 1) == ' ' && input.charAt(i) == ' ')
 				return i;
 		}
 		return -1;
--- src/test/java/com/pillar/PencilTest.java
+++ src/test/java/com/pillar/PencilTest.java
@@ -6,76 +6,83 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PencilTest {
-	
+
 	Pencil pencil;
-	
+
 	@Before
 	public void setUp() {
 		pencil = new Pencil(10, 40000, 1000);
 	}
-	
-	//Tests for write() function
+
+	// Tests for write() function
 	@Test
 	public void whenPencilIsPassedTwoStringsToWriteItReturnsAString() {
-		assertEquals("input", pencil.write("input",""));
+		assertEquals("input", pencil.write("input", ""));
 	}
-	
+
 	@Test
 	public void whenPencilIsPassedTwoStringsToWriteItReturnsTheStringsAppendedTogether() {
-		assertEquals("She sells sea shells down by the sea shore", pencil.write("She sells sea shells", " down by the sea shore"));
+		assertEquals("She sells sea shells down by the sea shore",
+				pencil.write("She sells sea shells", " down by the sea shore"));
 	}
-	
+
 	@Test
 	public void whenPencilWithoutAPointWritesAnythingItReturnsTheOriginalString() {
 		Pencil noPointPencil = new Pencil(10, 0, 1000);
 		assertEquals("input ", noPointPencil.write("input", " output"));
 		assertEquals(0, noPointPencil.getPointDurability());
 	}
-	
-	//Tests for pencil point degradation
+
+	@Test
+	public void whenPencilDegradesDuringWritingItReturnsAPartialString() {
+		Pencil minimalPointPencil = new Pencil(10, 4, 1000);
+		assertEquals("input outp", minimalPointPencil.write("input", " output"));
+	}
+
+	// Tests for pencil point degradation
 	@Test
 	public void whenPencilWritesSomethingItDegradesThePoint() {
 		int prevPointDurability = pencil.getPointDurability();
 		pencil.write("input", " output");
 		assertTrue(prevPointDurability > pencil.getPointDurability());
 	}
-	
+
 	@Test
 	public void whenPencilWritesALowercaseLetterItDegradesbyOne() {
 		int prevPointDurability = pencil.getPointDurability();
 		pencil.write("input", "a");
 		assertTrue(prevPointDurability == (pencil.getPointDurability() + 1));
 	}
-	
+
 	@Test
 	public void whenPencilWritesTwoLowercaseLettersItDegradesbyTwo() {
 		int prevPointDurability = pencil.getPointDurability();
 		pencil.write("input", "aa");
 		assertTrue(prevPointDurability == (pencil.getPointDurability() + 2));
 	}
-	
+
 	@Test
 	public void whenPencilWritesAnUpperCaseLetterItDegradesbyTwo() {
 		int prevPointDurability = pencil.getPointDurability();
 		pencil.write("input", "A");
 		assertTrue(prevPointDurability == (pencil.getPointDurability() + 2));
 	}
-	
+
 	@Test
 	public void whenPencilWritesTwoUpperCaseLettersItDegradesbyFour() {
 		int prevPointDurability = pencil.getPointDurability();
 		pencil.write("input", "AA");
 		assertTrue(prevPointDurability == (pencil.getPointDurability() + 4));
-		
+
 	}
 
 	@Test
 	public void whenPencilWritesAnUpperCaseAndLowercaseItDegradesByThree() {
 		int prevPointDurability = pencil.getPointDurability();
 		pencil.write("input", "Aa");
-		assertTrue(prevPointDurability == (pencil.getPointDurability() + 3));		
+		assertTrue(prevPointDurability == (pencil.getPointDurability() + 3));
 	}
-	
+
 	@Test
 	public void whenPencilWritesASpaceItDoesNotDegrade() {
 		int prevPointDurability = pencil.getPointDurability();
@@ -83,7 +90,8 @@ public class PencilTest {
 		assertTrue(prevPointDurability == (pencil.getPointDurability()));
 	}
 	
-	//Tests for sharpen() function
+
+	// Tests for sharpen() function
 	@Test
 	public void whenPencilIsSharpenedItsPointDurabilityReturnsToInitialValue() {
 		int prevPointDurability = pencil.getPointDurability();
@@ -91,14 +99,14 @@ public class PencilTest {
 		pencil.sharpen();
 		assertTrue(prevPointDurability == (pencil.getPointDurability()));
 	}
-	
+
 	@Test
 	public void whenPencilIsSharpenedItsLengthIsReducedByOne() {
 		int prevPencilLength = pencil.getLength();
 		pencil.sharpen();
 		assertTrue(prevPencilLength == (pencil.getLength() + 1));
 	}
-	
+
 	@Test
 	public void whenPencilLengthBecomesZeroItsPointDurabilityCannotBeRestored() {
 		Pencil pencilStub = new Pencil(0, 10000, 1000);
@@ -107,97 +115,97 @@ public class PencilTest {
 		pencilStub.sharpen();
 		assertTrue(prevPointDurability > (pencilStub.getPointDurability()));
 	}
-	
-	//Test for erase()
+
+	// Test for erase()
 	@Test
 	public void whenPencilErasesNothingThePaperDoesNotChange() {
-		assertEquals("input", pencil.erase("input",""));
+		assertEquals("input", pencil.erase("input", ""));
 	}
-	
+
 	@Test
 	public void whenPencilErasesAWordItReturnsTheInputWithSpacesWhereTheWordWas() {
-		assertEquals("input     output", pencil.erase("input and output","and"));
+		assertEquals("input     output", pencil.erase("input and output", "and"));
 	}
-	
+
 	@Test
 	public void whenPencilErasesAWordWithMultipleOccurrencesItErasesTheLastOccurrence() {
-		assertEquals("How much wood would a woodchuck chuck if a woodchuck could       wood?", 
-				pencil.erase("How much wood would a woodchuck chuck if a woodchuck could chuck wood?","chuck"));
+		assertEquals("How much wood would a woodchuck chuck if a woodchuck could       wood?",
+				pencil.erase("How much wood would a woodchuck chuck if a woodchuck could chuck wood?", "chuck"));
 	}
-	
-	//Test for eraser degradation
+
+	// Test for eraser degradation
 	@Test
 	public void whenPencilErasesSomethingItDegradesTheEraser() {
 		int prevEraserDurability = pencil.getEraserDurability();
-		pencil.erase("input and output and input","and");
+		pencil.erase("input and output and input", "and");
 		assertTrue(prevEraserDurability > pencil.getEraserDurability());
 	}
-	
+
 	@Test
 	public void whenPencilErasesALetterItDegradesTheEraserByOne() {
 		int prevEraserDurability = pencil.getEraserDurability();
-		pencil.erase("input and output and input","a");
+		pencil.erase("input and output and input", "a");
 		assertTrue(prevEraserDurability == (pencil.getEraserDurability() + 1));
 	}
-	
+
 	@Test
 	public void whenPencilErasesTwoLettersItDegradesTheEraserByTwo() {
 		int prevEraserDurability = pencil.getEraserDurability();
-		pencil.erase("input and output and input","an");
+		pencil.erase("input and output and input", "an");
 		assertTrue(prevEraserDurability == (pencil.getEraserDurability() + 2));
 	}
-	
+
 	@Test
 	public void whenPencilErasesASpaceItDoesNotDegrade() {
 		int prevEraserDurability = pencil.getEraserDurability();
-		pencil.erase("input and output and input"," ");
+		pencil.erase("input and output and input", " ");
 		assertTrue(prevEraserDurability == pencil.getEraserDurability());
 	}
-	
+
 	@Test
 	public void whenPencilNoEraserErasesItReturnsTheInput() {
 		Pencil noEraserPencil = new Pencil(10, 1000, 0);
-		assertEquals("input and output", noEraserPencil.erase("input and output","and"));
+		assertEquals("input and output", noEraserPencil.erase("input and output", "and"));
 	}
-	
+
 	@Test
 	public void whenPencilEraserRunsOutItErasesOnlyPartOfInput() {
 		Pencil lowEraserPencil = new Pencil(10, 1000, 3);
-		assertEquals("this str    has many words", lowEraserPencil.erase("this string has many words","string"));
+		assertEquals("this str    has many words", lowEraserPencil.erase("this string has many words", "string"));
 	}
-	
-	//Tests for edit() function
+
+	// Tests for edit() function
 	@Test
 	public void whenAPencilEditsEmptySpaceWithNothingItReturnsTheInput() {
-		String input = pencil.erase("this string has many words","string");
-		assertEquals("this        has many words",pencil.edit(input, ""));
+		String input = pencil.erase("this string has many words", "string");
+		assertEquals("this        has many words", pencil.edit(input, ""));
 	}
-	
+
 	@Test
 	public void whenAPencilEditsEmptySpaceWithALetterItReturnsTheInputWithTheLetterInTheEmptySpace() {
-		String input = pencil.erase("this string has many words","string");
-		assertEquals("this a      has many words",pencil.edit(input, "a"));
+		String input = pencil.erase("this string has many words", "string");
+		assertEquals("this a      has many words", pencil.edit(input, "a"));
 	}
-	
+
 	@Test
 	public void whenAPencilEditsEmptySpaceWithAShortWordItReturnsTheInputWithTheShortWordInTheEmptySpace() {
-		String input = pencil.erase("this string has many words","string");
-		assertEquals("this key    has many words",pencil.edit(input, "key"));
+		String input = pencil.erase("this string has many words", "string");
+		assertEquals("this key    has many words", pencil.edit(input, "key"));
 	}
-	
+
 	@Test
 	public void whenAPencilEditsEmptySpaceWithALongWordItReturnsTheInputWithTheLongWordInTheEmptySpace() {
-		String input = pencil.erase("this string has many words","string");
-		assertEquals("this paragra@@s many words",pencil.edit(input, "paragraph"));
+		String input = pencil.erase("this string has many words", "string");
+		assertEquals("this paragra@@s many words", pencil.edit(input, "paragraph"));
 	}
-	
+
 	@Test
 	public void whenDifferentPencilEditsEmptypSpaceItReturnsInputWithEmptySpaceEditedCorrectly() {
 		String input = pencil.erase("An apple a day keeps the doctor away", "apple");
 		Pencil diffPencil = new Pencil(5, 2000, 500);
 		assertEquals("An artich@k@ay keeps the doctor away", diffPencil.edit(input, "artichoke"));
 	}
-	
+
 	@Test
 	public void whenPencilEditsStringWithoutBlankSpaceItReturnsTheOriginalString() {
 		String input = "An apple a day keeps the doctor away";
